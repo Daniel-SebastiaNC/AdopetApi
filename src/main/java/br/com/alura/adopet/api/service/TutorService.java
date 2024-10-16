bd4f5e77d88779f1b0bf005ae365d58b15ca2e12
Daniel Sebastian Nunes Cavalcanti <daniel.sebastian.nc@gmail.com>
2024-10-16 10:33:48 -0300

    Tutor refac

---
+++ src/main/java/br/com/alura/adopet/api/service/TutorService.java
@@ -0,0 +1,32 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.AtualizacaoTutorDto;
+import br.com.alura.adopet.api.dto.CadastroTutorDto;
+import br.com.alura.adopet.api.excpetion.ValidacaoExcpetion;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TutorService {
+
+    @Autowired
+    private TutorRepository repository;
+
+    public void cadastrar(CadastroTutorDto dto){
+        boolean JaCadastrado = repository.existsByTelefoneOrEmail(dto.telefone(), dto.email());
+
+        if (JaCadastrado) {
+            throw new ValidacaoExcpetion("Dados já cadastrados para outro tutor!");
+        } else {
+            repository.save(new Tutor(dto));
+
+        }
+    }
+
+    public void atualizar(AtualizacaoTutorDto dto) {
+        Tutor tutor = repository.getReferenceById(dto.id());
+        tutor.atualizarDados(dto);
+    }
+}
